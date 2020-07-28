392e90a7b882e664b201dc9218677cfe25629d25
ishmi <ishanichowdhury7@gmail.com>
2020-07-28 16:56:53 -0700

    one if

--- General/foobar.py
+++ General/foobar.py
@@ -1,4 +1,5 @@
 # Print foo is multiple of 3, bar if multiple of 5 and foobar if multiple of 15
+'''
 print("how far do you want to go?")
 num = int(input())
 for j in range (1, num + 1):
@@ -12,5 +13,18 @@ for j in range (1, num + 1):
             print("bar")
         else:
             print(j)
+   ''' 
     
+# Print foo is multiple of 3, bar if multiple of 5 and foobar if multiple of 15
+print("how far do you want to go?")
+num = int(input())
+for j in range (1, num + 1):
+    if j % 15 == 0:
+         print("foobar")
+    elif j % 3 == 0:
+         print("foo")
+    elif j % 5 == 0:
+         print("bar")
+    else:
+         print(j)
     
